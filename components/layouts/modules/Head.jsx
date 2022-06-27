9a2a8bc6a9e4f06e58a940769d0e0c58b08ae14b
Berk Cohadar <berkcohadar1@gmail.com>
2022-06-27 05:42:12 -0400

    HTTP Call perm added. Logo updated.

--- components/layouts/modules/Head.jsx
+++ components/layouts/modules/Head.jsx
@@ -4,10 +4,10 @@ import Head from 'next/head';
 const StyleSheets = () => (
     <Head>
         <title>Partisepeti - React</title>
-        <link rel="shortcut icon" href="/static/img/favi.png" />
-        <link rel="icon" href="/static/img/favi.png" sizes="32x32" />
-        <link rel="icon" href="/static/img/favi.png" sizes="192x192" />
-        <link rel="apple-touch-icon-precomposed" href="/static/img/favi.png" />
+        <link rel="shortcut icon" href="/static/img/logo.png" />
+        <link rel="icon" href="/static/img/logo.png" sizes="32x32" />
+        <link rel="icon" href="/static/img/logo.png" sizes="192x192" />
+        <link rel="apple-touch-icon-precomposed" href="/static/img/logo.png" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <meta name="format-detection" content="telephone=no" />
@@ -15,6 +15,7 @@ const StyleSheets = () => (
         <meta name="author" content="nouthemes" />
         <meta name="keywords" content="Partisepeti, React" />
         <meta name="description" content="Partisepeti - React" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
         <link
             href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&amp;amp;subset=latin-ext"
             rel="stylesheet"

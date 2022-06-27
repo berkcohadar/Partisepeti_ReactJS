0b9766c4e3a9134febb0b55d59ab35db02c4f2dd
Berk Cohadar <berkcohadar1@gmail.com>
2022-06-27 05:06:19 -0400

    ip added

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-// const baseDomain = 'http://165.22.85.114/api'; // API for products
-const baseDomain = 'http://192.168.0.208:8000/api'; // API for products
+// const baseDomain = 'http://192.168.0.208:8000/api'; // API for products
+
+const baseDomain = 'http://164.92.140.87:1337/api'; // API for products
 
 
 export const customHeaders = {

3591c9fa0bc781f0dad8b2626f0dd62cdb7cecdd
Berk Cohadar <54453185+berkcohadar@users.noreply.github.com>
2022-07-03 01:44:51 -0400

    Update PanelCategories.jsx

--- components/shared/panel/PanelCategories.jsx
+++ components/shared/panel/PanelCategories.jsx
@@ -1,56 +1,61 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Menu } from 'antd';
 import Link from 'next/link';
 
 const { SubMenu } = Menu;
 
-class PanelCategories extends Component {
-    constructor(props) {
-        super(props);
-    }
+const PanelCategories = () => {
+    const [openKeys,setOpenKeys] = useState(['sub1']);
+    const [categories, setCategories] = useState([]);
+    const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
 
-    rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
-
-    state = {
-        openKeys: ['sub1'],
-    };
-    onOpenChange = openKeys => {
-        const latestOpenKey = openKeys.find(
-            key => this.state.openKeys.indexOf(key) === -1
+    const onOpenChange = keys => {
+        const latestOpenKey = keys.find(
+            key => openKeys.indexOf(key) === -1
         );
-        if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-            this.setState({ openKeys });
+        if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            setOpenKeys(openKeys);
         } else {
-            this.setState({
-                openKeys: latestOpenKey ? [latestOpenKey] : [],
-            });
+            setOpenKeys(
+                latestOpenKey ? [latestOpenKey] : [],
+            );
         }
     };
-    categories = [
-        {id:"1",slug:"/uyelik/siparislerim",name:"Siparişlerim"},
-        {id:"2",slug:"/uyelik/koleksiyonlarim",name:"Koleksiyonlarım"},
-        {id:"3",slug:"/uyelik/uyelik-bilgilerim",name:"Üyelik Bilgilerim"},
-        {id:"4",slug:"uyelik/adreslerim",name:"Adreslerim"},
-        // {id:"5",slug:"",name:""},
 
-    ];
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            if(JSON.parse(JSON.parse(localStorage.getItem('persist:partisepeti')).auth).isLoggedIn) {
+                setCategories(        
+                    [{id:"1",slug:"/uyelik/siparislerim",name:"Siparişlerim"},
+                {id:"2",slug:"/uyelik/koleksiyonlarim",name:"Koleksiyonlarım"},
+                {id:"3",slug:"/uyelik/uyelik-bilgilerim",name:"Üyelik Bilgilerim"},
+                {id:"4",slug:"uyelik/adreslerim",name:"Adreslerim"},
+                {id:"5",slug:"uyelik/yardim",name:"Yardim"}])
+            } else {
+                setCategories(        
+                    [{id:"1",slug:"/uyelik/giris",name:"Giriş"},
+                {id:"2",slug:"/uyelik/kayit",name:"Kayıt Ol"}])
+            }
+        }
+    }, [])
 
-    render() {
-        return (
-            <Menu
-                mode="inline"
-                openKeys={this.state.openKeys}
-                onOpenChange={this.onOpenChange}>
-                {this.categories.map(category => (
-                    <Menu.Item key={category.id} >
-                        <a href={`${category.slug}`}>
-                            {category.name}
-                        </a>
-                    </Menu.Item>
-                ))}
-            </Menu>
-        );
-    }
+    return (
+        <Menu
+            mode="inline"
+            openKeys={openKeys}
+            onOpenChange={() => onOpenChange()}>
+            {categories.map(category => (
+                <Menu.Item key={category.id} >
+                    <Link href={`${category.slug}`}>
+                    <a>
+                        {category.name}
+                    </a>
+                    </Link>
+
+                </Menu.Item>
+            ))}
+        </Menu>
+    );
 }
 
 export default PanelCategories;
